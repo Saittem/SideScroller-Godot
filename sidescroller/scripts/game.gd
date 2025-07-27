91196d545f2a406da2dc7eb3a357965b07db0888
Saittem <TEOLF@email.cz>
2025-07-27 20:56:37 +0200

    Added bird to obstacles (not finished)

--- sidescroller/scripts/game.gd
+++ sidescroller/scripts/game.gd
@@ -6,6 +6,7 @@ const START_SPEED : float = 2.0
 const MAX_SPEED : float = 15.0
 const SCORE_MODIFIER : int = 10
 const SPEED_MODIFIER : int = 8000
+const MAX_DIFFICULTY : int = 2
 
 var stump_scene = preload("res://scenes/stomp.tscn")
 var crate_scene = preload("res://scenes/crate.tscn")
@@ -13,7 +14,7 @@ var barrel_scene = preload("res://scenes/barrel.tscn")
 var bird_scene = preload("res://scenes/bird.tscn")
 var obstacle_types := [stump_scene, crate_scene, barrel_scene]
 var obstacles : Array
-var bird_heights := [200, 180]
+var bird_heights := [270, 240]
 var last_obs
 
 var score : float
@@ -21,11 +22,13 @@ var speed : float
 var screen_size : Vector2i
 var ground_height : int
 var game_running : bool
+var difficulty : int
 
 func _ready():
 	screen_size = get_window().size
 	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
+	difficulty = 0
 
 func new_game():
 	speed = 0
@@ -41,8 +44,9 @@ func _process(_delta):
 		if speed > MAX_SPEED:
 			speed = MAX_SPEED
 		score += speed
-		show_score()
 		
+		adjust_difficulty()
+		show_score()
 		generate_obstacles()
 		
 		$Player.position.x += speed
@@ -62,7 +66,7 @@ func generate_obstacles():
 	if obstacles.is_empty() or (last_obs.position.x + 200) < score + randi_range(300,500):
 		var obs_type = obstacle_types[randi() % obstacle_types.size()]
 		var obs 
-		var max_obs = 3
+		var max_obs = difficulty + 1
 		for i in range(randi() % max_obs + 1):
 			obs = obs_type.instantiate()
 			var obs_height = obs.get_node("Sprite2D").texture.get_height()
@@ -71,6 +75,18 @@ func generate_obstacles():
 			var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y / 4) + 10
 			last_obs = obs
 			add_obs(obs, obs_x, obs_y)
+		
+		if difficulty == 0: #MAX_DIFFICULTY:
+			if (randi() % 2) == 0:
+				obs = bird_scene.instantiate()
+				var obs_x : int = screen_size.x + score + 100
+				var obs_y : int = bird_heights[randi() % bird_heights.size()]
+				add_obs(obs, obs_x, obs_y)
+
+func adjust_difficulty():
+	difficulty = score / SPEED_MODIFIER
+	if difficulty > MAX_DIFFICULTY:
+		difficulty = MAX_DIFFICULTY
 
 func add_obs(obs, x, y):
 	obs.position =Vector2i(x, y)
